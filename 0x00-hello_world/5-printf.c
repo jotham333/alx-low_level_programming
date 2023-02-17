bfa98f731407fa5b06c489d85008d7ec2cfac2a7
jotham333 <jothamjosiah10@gmail.com>
2023-02-16 17:48:41 -0800

    ALX

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
+/*This code will print the said string*/
 int main(void)
 {
-	printf("with proper grammer, but the outcome is a piece of art\n");
+	printf("with proper grammer, but the outcome is a piece of art,\n");
 	return (0);
 }
--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 
-
 /**
  * main - Entry point
  *
@@ -8,5 +7,5 @@
  */
 int main(void)
 {
-	return(0);
+    return (0);
 }
