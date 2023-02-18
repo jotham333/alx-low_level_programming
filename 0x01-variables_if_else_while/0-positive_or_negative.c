2ea8a8bfe0f7d69931e18392c8054811aef21e5d
jotham333 <jothamjosiah10@gmail.com>
2023-02-18 01:11:28 -0800

    ALX

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,12 +10,17 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n != -n)
+	if (n > 0)
 	{
 		printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
 	}
 	else
 	{
 		printf("%d is negative\n", n);
-	}	return (0);
+	}
+	return (0);
 }
