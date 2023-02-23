0430ac08ad0efee722c2341918a96b28a17f63a4
jotham333 <jothamjosiah10@gmail.com>
2023-02-22 22:07:45 -0800

    ALX

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,14 +1,13 @@
 #include "main.h"
 #include <ctype.h>
 
-/*
- * _isalpha - checks if input is alphabet
+/**
+ *_isalpha - checks if input is alphabet
  *
  * @c: the character to be checked
  *
  * Return: 1 if c is lowercase, 0 otherwise
  */
-
 int _isalpha(int c)
 {
 	if (isalpha(c))
