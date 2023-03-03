ed9116a78518f419447d2431e50c9e34d75f2145
jotham333 <jothamjosiah10@gmail.com>
2023-03-03 15:29:20 -0800

    ALX

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -17,7 +17,8 @@ char *cap_string(char *a)
 
 	for (i = 0; a[i] != '\0'; i++)
 	{
-		if (i == 0 || a[i - 1] == ' ' || a[i - 1] == '\n')
+		if (i == 0 || a[i - 1] == ' ' || a[i - 1] == '\n' || a[i - 1] == '.'
+				|| a[i - 1] == '\t' || a[i -1] == '!')
 		{
 			a[i] = toupper(a[i]);
 		}
