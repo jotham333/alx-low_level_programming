58d0a23f2d30b576b814c4ea91c3cb7db94b94b1
jotham333 <jothamjosiah10@gmail.com>
2023-03-07 04:43:34 -0800

    ALX

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stddef.h>
+
+
+
+/**
+ *  *_strchr:  look for a character in a string
+ *
+ *@*s: the variable that contains the string
+ *
+ *@c the character to look for
+ *
+ *Return: the character if found un the string
+ *
+ *Return: NULL if not found
+ */
+
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			return &(s[i]);
+		}
+	}
+	return NULL;
+}
