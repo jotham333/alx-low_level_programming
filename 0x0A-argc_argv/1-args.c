2c18b4da7ecb2fdad6f19a81211e3c0eae51b1a9
jotham333 <jothamjosiah10@gmail.com>
2023-03-12 09:13:21 -0700

    ALX

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+
+/**
+ *main - name of the function
+ *
+ * @argc: the arguement counter
+ *
+ * @argv: note used
+ *
+ *Return: always 0
+ */
+
+
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+
+	_putchar(argc + '0');
+	_putchar('\n');
+	return (argc);
+}
