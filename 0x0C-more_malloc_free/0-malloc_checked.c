0687f4ac65350fc8083eba50270c5b48d495d17d
jotham333 <jothamjosiah10@gmail.com>
2023-03-17 21:46:06 -0700

    ALX

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+
+/**
+ *malloc_checked - allocate memory for input
+ *
+ *@b: the parameter
+ *
+ *Return: a if successful and NULL otherwise
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *a;
+
+	a = malloc(b);
+	if (a == NULL)
+	{
+		return (NULL);
+	}
+	return (a);
+}
---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,52 @@
+#include "main.h"
+#include <string.h>
+#include <unistd.h>
+
+
+/**
+ *string_nconcat - concatenate two variable into an allocated variable
+ *
+ *@s1: first parameter
+ *
+ *@s2: second parameter
+ *
+ *@n: number of byte of s2
+ *
+ *Return: a if successful and NULL otherwise
+ */
+
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i, j, k, len;
+	char *a;
+
+	k = strlen(s2) - n;
+	len = strlen(s1) + (strlen(s2) - k) + 1;
+	a = malloc(sizeof(char) * len);
+
+	if (a == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		a[i] = s1[i];
+	}
+
+	for (j = 0; j < (strlen(s2) - k); j++)
+	{
+		if (s2 == NULL)
+		{
+			a[strlen(s1) + j] = '\0';
+		}
+
+		else
+		{
+			a[strlen(s1) + j] = s2[j];
+		}
+	}
+	a[len - 1] = '\0';
+	return (a);
+}
