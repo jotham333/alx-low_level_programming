097015b4b92ba41f01e7003c247347e562462c99
jotham333 <jothamjosiah10@gmail.com>
2023-03-22 15:38:20 -0700

    ALX

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,5 @@
 #include "function_pointers.h"
-
+#include <stdio.h>
 
 /**
  * print_name - to print name
@@ -12,5 +12,9 @@
 
 void print_name(char *name, void (*f)(char *))
 {
+	if (name == NULL || f == NULL)
+	{
+	return;
+	}
 	f(name);
 }
--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,5 +1,6 @@
 #include "function_pointers.h"
 #include <stddef.h>
+#include <stdio.h>
 
 /**
  *array_iterator - the function name
@@ -15,6 +16,10 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
+	if (array == NULL || action == NULL)
+	{
+		return;
+	}
 	for (i = 0; i < size; i++)
 	{
 		action(array[i]);
