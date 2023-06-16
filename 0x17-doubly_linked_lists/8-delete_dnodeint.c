a096f38f71febe1997d45bc8e8d532eee49da6a6
Jotham333 <jothamjosiah10@gmail.com>
2023-06-16 04:50:58 +0000

    ALX

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -19,9 +19,8 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	if (h1 != NULL)
 		while (h1->prev != NULL)
 			h1 = h1->prev;
-
 	i = 0;
-	
+
 	while (h1 != NULL)
 	{
 		if (i == index)
@@ -39,7 +38,6 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 				if (h1->next != NULL)
 					h1->next->prev = h2;
 			}
-
 			free(h1);
 			return (1);
 		}
@@ -47,6 +45,5 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		h1 = h1->next;
 		i++;
 	}
-
 	return (-1);
 }
