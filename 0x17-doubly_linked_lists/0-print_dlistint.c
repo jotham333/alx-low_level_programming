b600aed1604b670097d7b10188e9faad20740d51
Jotham333 <jothamjosiah10@gmail.com>
2023-06-16 01:52:53 +0000

    ALX

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -10,13 +10,16 @@
 size_t print_dlistint(const dlistint_t *h)
 {
     const dlistint_t *ptr = h;
-    
+    size_t count = 0;
+  
     while (ptr != NULL)
     {
-        printf("%d", ptr->n);
+        printf("%d\n", ptr->n);
         ptr = ptr->next;
-	putchar('\n');
+	count++;
     }
-    return (0);
+
+    printf("-> %lu element\n", count);
+    return (count);
 }
 
